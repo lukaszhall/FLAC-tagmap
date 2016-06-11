ef41607eef5d5fbf510b15c3f020ff845d318f4d
lukaszhall <hall@alum.mit.edu>
2016-06-11 19:18:45 -0400

    Project create

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to flac-tagmap
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/flac_tagmap/core_test.clj
@@ -0,0 +1,7 @@
+(ns flac-tagmap.core-test
+  (:require [clojure.test :refer :all]
+            [flac-tagmap.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
