86db7b12f51f5d2e27ad6a6cef63f7185b91cf7c
lukaszhall <hall@alum.mit.edu>
2016-06-12 04:54:29 -0400

    ID3 reads and library additions

--- project.clj
+++ project.clj
@@ -3,7 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [environ "1.0.3"]
+                 [com.taoensso/timbre "4.4.0"]
+                 [net.jthink/jaudiotagger "2.2.3"]]
+  :repositories {"bintray" "https://dl.bintray.com/ijabz/maven"}
+  :plugins [[lein-environ "1.0.3"]
+            [lein-pprint "1.1.1"]]
   :main ^:skip-aot flac-tagmap.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/flac_tagmap/core.clj
+++ src/flac_tagmap/core.clj
@@ -1,7 +1,71 @@
 (ns flac-tagmap.core
+  (:require [environ.core :refer [env]]
+            [taoensso.timbre :as timbre
+             :refer (log  trace  debug  info  warn  error  fatal  report
+                     logf tracef debugf infof warnf errorf fatalf reportf
+                     spy get-env log-env)])
+  (:import (org.jaudiotagger.tag.flac FlacTag)
+           (org.jaudiotagger.audio    AudioFileIO)
+           (org.jaudiotagger.tag      FieldKey))
   (:gen-class))
 
+;(timbre/set-level! :)
+
+(def flac-loc (env :flac-read-dir))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Program entry."
   [& args]
-  (println "Hello, World!"))
+  (println (str  "Reading from flac dir:" flac-loc ))
+;  (debug (Exception. "Uh Oh!") "some arg" 2)
+  (println (str "flac-dir:" (env :flac-src-dir))))
+
+
+(def flac-src-dir (env :flac-src-dir))
+
+
+(defn id3-vals
+  "read all ID3 tags for a FLAC file"
+  [file]
+  (let [audio-file (AudioFileIO/read file)
+        tag        (.getTag  audio-file)
+        header     (.getAudioHeader audio-file)
+        fields     {:album        FieldKey/ALBUM
+                    :album-artist FieldKey/ALBUM_ARTIST
+                    :artist       FieldKey/ARTIST
+                    :bpm          FieldKey/BPM
+                    :composer     FieldKey/COMPOSER
+                    :disc         FieldKey/DISC_NO
+                    :disc-total   FieldKey/DISC_TOTAL
+                    :genre        FieldKey/GENRE
+                    :title        FieldKey/TITLE
+                    :track        FieldKey/TRACK
+                    :track-total  FieldKey/TRACK_TOTAL
+                    :year         FieldKey/YEAR}]
+    (loop [field-seq (seq fields)
+           field-map {}]
+      (if (= 0 (count field-seq))
+        field-map
+        (let [next-field  (first field-seq)
+              next-type   (first next-field)
+              next-id3key (second next-field)]
+          (recur (rest field-seq)
+                 (assoc field-map
+                        next-type
+                        (.getFirst tag next-id3key))))))))
+
+
+
+(defn find-flac-files
+  "Return a sequence of all flac files in a given directory"
+  [dir]
+  (let [all-files  (clojure.java.io/file dir)
+        flac-files (->> (file-seq all-files)
+                        (filter #(clojure.string/ends-with? % ".flac")))]
+    flac-files))
+
+(defn song-composer-map
+  "Build song composer map for a given list of flac files"
+  [flac-files]
+  (let [all-id3-tags (map id3-vals flac-files)]
+    (loop [] true)))
